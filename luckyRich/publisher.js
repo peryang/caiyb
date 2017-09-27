65a7d06d2d717a4edf0baf956549efb52f6c2475
bjyangxiuwu <bjyangxiuwu@corp.netease.com>
2017-09-27 16:56:45 +0800

    luckyRich

---
+++ luckyRich/publisher.js
@@ -0,0 +1,18 @@
+//Publisher.js file Example. 
+//Last Update: November 2015 
+//First, let’s describe the game a bit. These are the variables 
+//you should reference in your Booster API initialization in 
+//your index.html 
+var gameName = "Shaman's Treasure";
+var gameVersion = "1.0"; 
+var gameCategory="Puzzle"; 
+var developerId="0703"; //Provided by Booster 
+var gameCode ="0703-shaman_treas"; 
+//Provided by Booster //Now, on to advertising settings. Please leave these 
+//placeholder values as-is when implementing. 
+var adChannel = "fog_0703-shaman_treas";
+var enableAds = true;
+var adFreq = "60";
+var firstAd = "0"; //Controlling More Games behavior… 
+var moreGames = 0;
+var moreGamesURL = "www.coolgames.com";
---
+++ luckyRich/texts.js
@@ -0,0 +1,41 @@
+﻿var g_strings =
+{
+	"ru":
+	{
+		"PAUSE":"Пауза",
+		"EARN": "Получите",
+		"POINTS": "очков",
+		"REMOVE": "Удалите",
+		"CLEAR":"Очисти все ячейки",
+		"ACHIEVEMENT":"Сокровища",
+		"TUTORIAL_1": "Уничтожьте три в ряд",
+		"TUTORIAL_2": "Уничтожьте 3 или 4 для создания бонуса",
+		"TUTORIAL_3": "Уничтожьте бонус с фишкой",
+		"TUTORIAL_4": "Уничьтожьте бонусы, чтобы создать шамана",
+		"TUTORIAL_5": "Уничьтожьте шамана с фишкой.",
+		"TOTAL_SCORE": "Всего очков:",
+		"NEW_TREASURE": "Открыто новое сокровище!",
+		"GJ_MESSAGE": "Поздравляем! Вы прошли все уровни!"
+	},	
+
+	"en":
+	{
+		"PAUSE":"PAUSE",
+		"EARN": "Score",
+		"POINTS": "points",
+		"REMOVE": "Remove",
+        "CLEAR": "Clear all dirty cells",
+		"ACHIEVEMENT":"Treasures",
+		"TUTORIAL_1": "Make a simple match",
+		"TUTORIAL_2": "Match 4 or 5 chips to generate bonuses.",
+		"TUTORIAL_3": "Match bonus with gem to activate it.",
+		"TUTORIAL_4": "Match bonuses with each other to generate shaman bonus.",
+		"TUTORIAL_5": "Match shaman with gem.",
+		"TOTAL_SCORE": "Total score:",
+		"NEW_TREASURE": "New treasure unlocked!",
+		"GJ_MESSAGE": "Congratulations! You have completed all levels!"
+	}
+	
+};
+
+var g_allLanguages = [ "en", "ru"];
